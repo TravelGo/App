2711c5f1a8b6e279105afbef1923bb9e6a7944ee
spectator05 <spectat@kookmin.ac.kr>
2018-11-18 16:00:17 +0900

    MyPage's tabView of react-native is implemented

--- MyPage.js
+++ MyPage.js
@@ -1,1 +1,45 @@
-/* empty file */
+import * as React from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
+
+const FirstRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
+const ThirdRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
+
+export default class TabViewExample extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'map', title: '지도' },
+      { key: 'TravelStop', title: '트레블 스탑' },
+      { key: 'Achieve', title: '도전 과제' },
+    ],
+  };
+
+  render() {
+    return (
+      <TabView
+        navigationState={this.state}
+        renderScene={SceneMap({
+          map: FirstRoute,
+          TravelStop: SecondRoute,
+          Achieve: ThirdRoute
+        })}
+        onIndexChange={index => this.setState({ index })}
+        initialLayout={{ width: Dimensions.get('window').width }}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+  },
+});
