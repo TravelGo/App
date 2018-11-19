12fe763530a88595a2fbfa5a4e3c24d0aa406639
spectator05 <spectat@kookmin.ac.kr>
2018-11-19 13:03:27 +0900

    tool bar + tab view with little error

--- MyPage.js
+++ MyPage.js
@@ -1,18 +1,20 @@
+
 import * as React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Button, Icon, TouchableOpacity, Image } from 'react-native';
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 
 const FirstRoute = () => (
-  <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+  <View style={[styles.scene, { backgroundColor: '#ffffff' }]} />
 );
 const SecondRoute = () => (
-  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+  <View style={[styles.scene, { backgroundColor: '#ffffff' }]} />
 );
 const ThirdRoute = () => (
-  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+  <View style={[styles.scene, { backgroundColor: '#ffffff' }]} />
 );
 
 export default class TabViewExample extends React.Component {
+  
   state = {
     index: 0,
     routes: [
@@ -22,18 +24,33 @@ export default class TabViewExample extends React.Component {
     ],
   };
 
+  _goToMap = () => { Actions.map() }
+  
   render() {
     return (
-      <TabView
-        navigationState={this.state}
-        renderScene={SceneMap({
-          map: FirstRoute,
-          TravelStop: SecondRoute,
-          Achieve: ThirdRoute
-        })}
-        onIndexChange={index => this.setState({ index })}
-        initialLayout={{ width: Dimensions.get('window').width }}
-      />
+      <View style={{flexDirection: 'column', flex: 1}}>
+        <View style={{paddingHorizontal: 15, padding: 35,
+          backgroundColor: '#109eff', height: 100, resizeMode: 'contain', flexDirection: 'row', justifyContent: 'space-between'}}>
+          <TouchableOpacity onPress={this._goToMap}>
+            <Image source={require("./images/goBackButton.png")} style={{width:30, height: 30, resizeMode: 'contain'}}/>
+          </TouchableOpacity>
+          <Text style={{color: 'white', fontSize: 30}}>마이페이지</Text>
+          <TouchableOpacity onPress={this._toggleModal}>
+            <Image source={require("./images/goBackButton.png")} style={{width:30, height: 30, resizeMode: 'contain'}}/>
+          </TouchableOpacity>
+        </View>
+        <TabView
+        
+          navigationState={this.state}
+          renderScene={SceneMap({
+            map: FirstRoute,
+            TravelStop: SecondRoute,
+            Achieve: ThirdRoute
+          })}
+          onIndexChange={index => this.setState({ index })}
+          initialLayout={{ width: Dimensions.get('window').width}}
+        />
+      </View>
     );
   }
 }
